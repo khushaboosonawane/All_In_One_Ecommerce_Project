4969d24dbd3ce34a9ddc34008075f78d934b1a5e
khushaboosonawane <sokhushaboo202@gmail.com>
2024-05-19 10:55:13 +0530

    Done

--- application/views/admin/navbar.php
+++ application/views/admin/navbar.php
@@ -5,8 +5,10 @@ defined("BASEPATH") or exit("no direct script is allowed");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>All In One Ecommerce</title>
+	<link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet"/>
 	<link href="<?= base_url() ?>public/admin_assets/css/modern.css" rel="stylesheet">
+	<link rel="icon" href="<?= base_url() ?>public/logo.jpg">
 	<style>
 		body {
 			opacity: 0;
@@ -31,11 +33,8 @@ defined("BASEPATH") or exit("no direct script is allowed");
 
 	<div class="wrapper">
 		<nav id="sidebar" class="sidebar">
-			<a class='sidebar-brand' href='<?= base_url() ?>/admincontroller/index'>
-				<svg>
-					<use xlink:href="#ion-ios-pulse-strong"></use>
-				</svg>
-				Spark
+			<a class='sidebar-brand text-center' href='<?= base_url() ?>/admincontroller/index'>
+				<img src="<?= base_url() ?>public/logo.jpg" alt="" style="height:75px;width:90px;border-radius:50%;">
 			</a>
 			<div class="sidebar-content">
 				<div class="sidebar-user">
@@ -73,14 +72,15 @@ defined("BASEPATH") or exit("no direct script is allowed");
 		</nav>
 		<div class="main">
 			<nav class="navbar navbar-expand navbar-theme">
-				<a class="sidebar-toggle d-flex me-2">
-					<i class="hamburger align-self-center"></i>
+				<a class="sidebar-toggle d-flex me-2" style="text-decoration:none">
+					<!-- <i class="hamburger align-self-center"></i> -->
+					<i class="ri-arrow-left-line text-white lead p-3"></i>
 				</a>
 				<div class="navbar-collapse collapse">
 					<ul class="navbar-nav ms-auto">
 						<li class="nav-item dropdown ms-lg-2">
 							<a class="nav-link dropdown-toggle position-relative" href="#" id="userDropdown" data-bs-toggle="dropdown">
-								<i class="align-middle fas fa-cog"></i>
+								<i class="ri-user-line lead" style="font-weight:bold"></i>
 							</a>
 							<div class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
 								<a class="dropdown-item" href="#"><i class="align-middle me-1 fas fa-fw fa-user"></i> View Profile</a>
