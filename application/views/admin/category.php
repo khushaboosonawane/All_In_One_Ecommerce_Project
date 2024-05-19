da820289bf48b66acf3e37fbcb9286f8dad2def8
khushaboosonawane <sokhushaboo202@gmail.com>
2024-05-19 11:25:11 +0530

    all changes done

--- application/views/admin/category.php
+++ application/views/admin/category.php
@@ -1,3 +1,43 @@
 <?php
 defined("BASEPATH") or exit("No direct script is allowed");
 ?>
+ <main class="content">
+    <div class="container-fluid">
+        <div class="header">
+            <h1 class="header-title">
+                Add Category
+            </h1>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href='<?= base_url() ?>admincontroller/index'>Dashboard</a></li>
+                    &nbsp;&nbsp; <span class="text-white fw-bold">/</span> &nbsp;&nbsp;
+                    <a href="<?= base_url() ?>admincontroller/category" class="text-white fw-bold">Category</a>
+                </ol>
+            </nav>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card" style="box-shadow:0 0 15px #fff">
+                    <div class="card-header">
+                        <h5 class="card-title lead fw-bold">Category form</h5>
+                    </div>
+                    <div class="card-body">
+                        <form class="row row-cols-md-auto align-items-center">
+                            <div class="col-12">
+                                <label class="visually-hidden" for="inlineFormInputGroupUsername2">Category Name</label>
+                                <div class="input-group mb-2 me-sm-2">
+                                    <div class="input-group-text bg-dark text-white">Category Name</div>
+                                    <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Enter Category Name">
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-dark mb-2" style="background-color:black">Save Information</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+</main>
